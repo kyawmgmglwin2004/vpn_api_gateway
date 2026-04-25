8ae1e73db0674428e70947d232b46f565e1bd360
mac <mac@macui-MacBookPro.local>
2026-04-25 14:44:34 +0630

    update allow

--- src/features/users/users_controller.js
+++ src/features/users/users_controller.js
@@ -5,7 +5,7 @@ async function userLogin(req, res) {
     try {
         const { phone, password } = req.body;
         const result = await usersService.userLogin(phone, password);
-        res.status(result.status).json(result);   
+        res.status(result.code).json(result);   
     } catch (error) {
         console.error("Error user register action:", error);
 
@@ -18,7 +18,7 @@ async function userRegister(req, res) {
     try {
         const { name, phone, password } = req.body;
         const result = await usersService.userRegister(name, phone, password);
-        res.status(result.status).json(result);   
+        res.status(result.code).json(result);   
     } catch (error) {
         console.error("Error user register action:", error);
 
@@ -30,7 +30,7 @@ async function getUserInfo(req, res) {
     try {
         const userId = req.user.id;
         const result = await usersService.getUserInfo(userId);
-        res.status(result.status).json(result);   
+        res.status(result.code).json(result);   
     } catch (error) {
         console.error("Error user register action:", error);
 
